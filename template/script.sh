572c177c8e5b7f9a3779178db7572ba6eaed5b6a
Jeremy Nicklas <jnicklas@osc.edu>
2017-05-31 14:12:05 -0400

    refactor to support new batch_connect

--- template/script.sh
+++ template/script.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -l
 
+# Get current working directory
+export OUTPUT_ROOT="${PWD}"
+
+# Get source directory of this script
+export STAGED_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 # Set working directory to home directory
 cd "${HOME}"
-
-# Restore the module environment to avoid conflicts
-module restore
 
 #
 # Launch Fluxbox
 #
 
-FLUXBOX_RC_FILE="${PBS_O_WORKDIR}/${PBS_JOBID}.rc"
-FLUXBOX_ASSETS_ROOT="${ROOT}/fluxbox"
+FLUXBOX_RC_FILE="${OUTPUT_ROOT}/fluxbox.rc"
+FLUXBOX_ASSETS_ROOT="${STAGED_ROOT}/fluxbox"
 
 # Build Fluxbox init file
 cat > "${FLUXBOX_RC_FILE}" << EOT
@@ -26,18 +29,25 @@ EOT
 # Export the module function for the terminal
 [[ $(type -t module) == "function" ]] && export -f module
 
-# Start the Fluxbox window manager
-FLUXBOX_ASSETS_ROOT="${FLUXBOX_ASSETS_ROOT}" fluxbox -display "${DISPLAY}.0" -rc "${FLUXBOX_RC_FILE}" &
+# Start the Fluxbox window manager (it likes to crash on occassion, make it
+# persistent)
+(
+  export FLUXBOX_ASSETS_ROOT="${FLUXBOX_ASSETS_ROOT}"
+  until fluxbox -display "${DISPLAY}.0" -rc "${FLUXBOX_RC_FILE}"; do
+    echo "Fluxbox crashed with exit code $?. Respawning..." >&2
+    sleep 1
+  done
+) &
 
 #
 # Start MATLAB
 #
+
+# Restore the module environment to avoid conflicts
+module restore
 
 # Load the MATLAB module
 module load ${MATLAB_MODULE}
 
 # Launch MATLAB
 matlab -desktop
-
-# Kill vncserver when user closes GUI
-vncserver -kill ${DISPLAY}
