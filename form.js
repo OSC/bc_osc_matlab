6d5ea9015bfc97da9060bd13683a9695cdbc8257
Morgan Rodgers <mrodgers@osc.edu>
2019-02-21 13:31:34 -0500

    Fix front end to allow ppn control to remain blank

--- form.js
+++ form.js
@@ -1,14 +1,18 @@
 'use strict'
 
 /**
- * Fix num cores
+ * Fix num cores, allowing blanks to remain
  */
 function fix_num_cores() {
   let node_type_input = $('#batch_connect_session_context_node_type');
   let node_type = node_type_input.val();
   let num_cores_input = $('#num_cores');
-  
-  if(node_type === ':hugemem') {
+
+  if(num_cores_input.val() === '') {
+    return;
+  }
+
+  if(node_type === 'hugemem') {
     set_ppn_owens_hugemem(num_cores_input);
   } else {
     set_ppn_owens_regular(num_cores_input);
